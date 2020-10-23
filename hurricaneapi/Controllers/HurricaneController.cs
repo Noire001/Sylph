92592ca156d03ada1744189330cb7cc6b770bf49
Konstantinos Karakostas <konstantk@csd.auth.gr>
2020-10-23 15:13:15 +0300

    initial commit

---
+++ hurricaneapi/Controllers/HurricaneController.cs
@@ -0,0 +1,26 @@
+﻿using hurricaneapi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hurricaneapi.Controllers
+{
+    [Route("[controller]")]
+    [Controller]
+    public class HurricaneController : ControllerBase
+    {
+        private HurricaneService hurricaneService { get; }
+        public HurricaneController(HurricaneService hurricaneService)
+        {
+            this.hurricaneService = hurricaneService;
+        }
+        // GET
+        [Route("fetch")]
+        [HttpGet]
+        public ActionResult Get()
+        {
+            hurricaneService.FetchFiles();
+            return Ok();
+        }
+
+        
+    }
+}
---
+++ hurricaneapi/Services/HurricaneService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace hurricaneapi.Services
+{
+    public class HurricaneService
+    {
+        public void FetchFiles()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile("https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.ACTIVE.list.v04r00.csv","file.csv");
+                
+            }
+        }
+    }
+}
